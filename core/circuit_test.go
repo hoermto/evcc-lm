83b2591102ee4c4af0abedb2ef8da7daf3ce2857
hoermto <hoermto@thoermann.net>
2022-07-31 10:51:53 +0200

    add tests for hierarchy

--- core/circuit_test.go
+++ core/circuit_test.go
@@ -112,3 +112,40 @@ func TestCurrentCircuitPrio(t *testing.T) {
 	curAv := circ.GetRemainingCurrent()
 	assert.Equal(t, limit-mtr.cur, curAv)
 }
+
+func TestParentCircuit(t *testing.T) {
+	// two circuits, check limit and consumption from both sides
+	limitMain := 25.0
+	circMain := NewCircuit("testCircuitMain", limitMain, nil, util.NewLogger("test circuit Main"))
+	assert.NotNilf(t, circMain, "circuit not created")
+	circMain.Consumers = append(circMain.Consumers, &testConsumer{cur: 16})
+	// add subcircuit with meter
+	limitSub := 20.0
+	mtrSub := &testMeter{cur: 10.0} // consumption of subCircuit
+	circMain.Circuits = append(circMain.Circuits, *NewCircuit("testCircuitSub", limitSub, mtrSub, util.NewLogger("test circuit Sub")))
+	circSub := circMain.GetCircuit("testCircuitSub")
+	assert.NotNilf(t, circSub, "subcircuit not created")
+	circSub.parentCircuit = circMain
+
+	assert.NotNilf(t, circSub.parentCircuit, "parent circuit not set")
+	assert.NotNilf(t, circSub.meterCurrent, "sub circuit meter not set")
+	assert.Equal(t, circSub.GetCurrent(), 10.0)
+
+	// consumption of mainCircuit is the consumer from main + subcircuit consumption
+	assert.Equal(t, circMain.GetCurrent(), 16.0+10.0)
+
+	// remaining current of main: limit - consumption: 25 - (16+10)
+	// overload situation ...
+	assert.Equal(t, circMain.GetRemainingCurrent(), -1.0)
+
+	// remaining current of sub: limit - consumption.
+	// it considers the parent circuit limits. main is -1 ..., sub is 20-10. Lower wins
+	assert.Equal(t, circSub.GetRemainingCurrent(), -1.0)
+
+	// increase main limit, lower sub limit. sub has 2 left, so this applies
+	circMain.MaxCurrent += 5
+	circSub.MaxCurrent = 12 // 2 left
+	assert.Equal(t, circMain.GetRemainingCurrent(), 4.0)
+	assert.Equal(t, circSub.GetRemainingCurrent(), 2.0)
+
+}
